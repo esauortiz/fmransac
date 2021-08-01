f7b0166469ef54a8d61fae5741a4dd2a986f30a9
esauortiz <esauortiz97@gmail.com>
2021-08-01 18:32:55 +0200

    not important commit

--- runtest.sh
+++ runtest.sh
@@ -37,20 +37,20 @@ for i in $(seq $initial_batch $batches_num); do
 	mkdir -p ${save_path}/yaml
 	# Create master yaml: describes the batch params.
 	# Also saves the master yaml in each batch directory
-	python src/testcfg/gen_yaml_master_file.py ${save_path} batch_$i
+	python scripts/testcfg/gen_yaml_master_file.py ${save_path} batch_$i
 
 	echo 'Running test: 'batch_$i
 	echo 'Model class: '$model 
 	echo 'Number of tests: '${tests_num}
 
 	# Generate each test's yaml_file
-	python src/testcfg/gen_yaml_file.py batch_$i ${tests_num} ${save_path}
+	python scripts/testcfg/gen_yaml_file.py batch_$i ${tests_num} ${save_path}
 	echo 'Yaml files generated'
 
 	# Generating data only if current batch is master_batch or if there is none
 	if [ batch_$i == $master_batch ] || [ $master_batch == 'None' ]
 	then
-		python src/data/gen_data_${model}.py ${tests_num} ${save_path}
+		python scripts/data/gen_data_${model}.py ${tests_num} ${save_path}
 		echo 'Data generated'
 	# Else copy data from master batch in the current batch folder
 	else
@@ -59,12 +59,12 @@ for i in $(seq $initial_batch $batches_num); do
 	fi
 
 	# Estimate model params, default estimation will always be run
-	python src/estimation/estimate_model.py ${tests_num} ${save_path} ransac # {ransac,msac,fun1,fun2,fun3,fun4}
+	python scripts/estimation/estimate_model.py ${tests_num} ${save_path} ransac # {ransac,msac,fun1,fun2,fun3,fun4}
 	echo ''
 	echo 'Tests finished. Generating results... '
 
 	# Create results files
-	python src/estimation/estimation_results.py ${save_path} ${tests_num} ${model} ransac
+	#python scripts/estimation/estimation_results.py ${save_path} ${tests_num} ${model} ransac
 
 	echo 'Results generated: 'batch_$i
 	echo ' '
