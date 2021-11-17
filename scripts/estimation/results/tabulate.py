12a6a4bb1af768d4e8cee3ea96c22408b645f6d4
esauortiz <esauortiz97@gmail.com>
2021-11-17 10:27:20 +0100

    update estimation/results/tabulate.py

--- scripts/estimation/results/tabulate.py
+++ scripts/estimation/results/tabulate.py
@@ -1,16 +1,34 @@
+"""
+# Usage
+    python scripts/estimation/results/tabulate.py model_class group_id rows_labels metric stat_type
+
+# Args
+    model_class: model class could take values in ['PlaneModelND', 'EllipseModel', 'HomographyModel']
+    group_id: group identifier beginning with G
+    rows_labels: indicates which batch parameters should be used as rows labels in data frame. Must match number of batches (n_batches)
+    metric: results metric could take values in ['abs_errors', 'rel_errors', 'estimation_errors', 'iterations']
+    stat_type: statistic value based on results metric. Takes any percentile beginning with P (e.g. 'P95'), 'mean' or 'sd'
+
+# Example
+    python scripts/estimation/results/tabulate.py PlaneModelND G1 outlier_ratio estimation_errors P95
+"""
+
 from test_configuration.utils import _read_yaml
 from estimation.results.utils import get_metric
-
+from pathlib import Path
 import numpy as np
-import sys, os
+import pandas as pd
+import sys
 
-if __name__ == '__main__':
-
+def main():
     model_class = sys.argv[1]
     group_id = sys.argv[2]
-
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    scripts_path = current_path[:-19]
+    param_as_row_label = sys.argv[3]
+    metric = sys.argv[4]
+    stat_type = sys.argv[5]
+    
+    current_path = Path(__file__).parent.resolve()
+    scripts_path = current_path.parent.parent
     tests_path = _read_yaml(f'{scripts_path}/test_configuration/params/tests_path.yaml')['path']
 
     # read saved group params (batch_group_params could be modified but tests_path is expected not to change)
@@ -20,27 +38,34 @@ if __name__ == '__main__':
     estimators_names = batch_group_params['group_params']['estimators_names']
     n_estimators = len(estimators_names)
 
-    # read table params
-    table_params = _read_yaml(f'{scripts_path}/estimation/results/table_params.yaml')
-    metric = table_params['metric']
-    stat_type = table_params['stat_type']
+    # build results as data frame
+    data = np.empty((n_batches, n_estimators))
+    row_labels = []
 
-    table = np.empty((n_batches, n_estimators))
-
-    for batch_id in range(n_batches):
+    for batch_id in reversed(range(n_batches)):
         # read batch params
         batch_save_path = f'{tests_path}/{model_class}/batch_{initial_batch_id + batch_id}'
         batch_params = _read_yaml(f'{batch_save_path}/batch_params.yaml')            
-        n_tests = batch_params['n_tests']
-
+        try:
+            row_labels.append(batch_params['dataset_params'][param_as_row_label])
+        except KeyError:
+            row_labels.append(batch_params['ransac_params'][param_as_row_label])
+            
         batch_row = []
         for estimator in estimators_names:
             #read results
             results = np.loadtxt(f'{batch_save_path}/results/{estimator}/00_{metric}.txt')
             metric_value = get_metric(results, stat_type)
             batch_row.append(metric_value)
-        
-        table[batch_id] = batch_row
+
+        data[batch_id] = batch_row
+
+    results_df = pd.DataFrame(data, index=row_labels, columns=estimators_names)
     
     # save table
-    np.savetxt(f'{tests_path}/{model_class}/00_batch_groups/{group_id}/results_tables/{metric}_{stat_type}.txt', table)
+    print(results_df)
+    results_df.to_csv(f'{tests_path}/{model_class}/00_batch_groups/{group_id}/results_tables/{metric}_{stat_type}.csv')
+    print(f'Data frame has been saved in {tests_path}/{model_class}/00_batch_groups/{group_id}/results_tables/{metric}_{stat_type}.csv')
+
+if __name__ == '__main__':
+    main()
