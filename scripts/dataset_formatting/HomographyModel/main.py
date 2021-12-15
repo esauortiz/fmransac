8a0e33ff24b033cc8ba01d1720d67ba4b0923a1f
esauortiz <esauortiz97@gmail.com>
2021-12-15 09:44:19 +0100

    dataset_formatting/HomographyModel/main.py: using provided correspondences

--- scripts/dataset_formatting/HomographyModel/main.py
+++ scripts/dataset_formatting/HomographyModel/main.py
@@ -34,7 +34,8 @@ if __name__ == '__main__':
     group_id = sys.argv[2]
 
     # directories and constants
-    DATASET_PATH = f'/media/esau/hdd_at_ubuntu/fm_packages/fm_matching/tests/datasets/{dataset_label}'
+    #DATASET_PATH = f'/media/esau/hdd_at_ubuntu/fm_packages/fm_matching/tests/datasets/{dataset_label}'
+    DATASET_PATH = f'/home/esau/tfm/datasets/HomographyModel/{dataset_label}'
     GROUP_PATH = f'/home/esau/tfm/tests/HomographyModel/00_batch_groups/{group_id}'
     BATCH_PATH = '/home/esau/tfm/tests/HomographyModel'
     MIN_MATCH_COUNT = 4
@@ -50,23 +51,21 @@ if __name__ == '__main__':
     # format data for each image pair
     for figure_label, idx in zip(figures_dict, range(n_batches)):
         figure_ext = figures_dict[figure_label]
+        """
         # load image pair
         fname1 = f'{DATASET_PATH}/{figure_label}A.{figure_ext}'
         fname2 = f'{DATASET_PATH}/{figure_label}B.{figure_ext}'
         img1 = cv.imread(fname1,0) # trainImage
         img2 = cv.imread(fname2,0) # queryImage
         # Initiate SIFT detector
-        sift = cv.SIFT_create()
+        sift = cv.SIFT_create(nfeatures = 10000, contrastThreshold = 0.01, edgeThreshold = 40)
         #surf = cv.SURF_create()
         #brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
 
         # from keypoints to descriptors, but descriptors are conditioned to manually set kp.size
         kp1, des1 = sift.detectAndCompute(img1, None)
         kp2, des2 = sift.detectAndCompute(img2, None)
-
-        # keypoints and its descriptors from ground truth
-        kp1_ori, kp2_ori, M_ori = read_validation(DATASET_PATH, figure_label, as_keypoints = False)
-
+    
         # matching w/ brute force
         #bf = cv.BFMatcher_create()
         #matches = bf.match(des1, des2)
@@ -86,7 +85,7 @@ if __name__ == '__main__':
 
         # ratio test as per Lowe's paper
         for i,(m,n) in enumerate(matches):
-            if m.distance < 0.8*n.distance:
+            if m.distance < 0.6*n.distance:
                 good.append(m)
                 pts2.append(kp2[m.trainIdx].pt)
                 pts1.append(kp1[m.queryIdx].pt)
@@ -98,7 +97,10 @@ if __name__ == '__main__':
             dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ])
         else:
             print( "Not enough matches are found - {}/{}".format(len(matches), MIN_MATCH_COUNT) )
-        
+        """
+        # keypoints and its descriptors from ground truth
+        kp1_ori, kp2_ori, src_pts, dst_pts, M_ori = read_validation(DATASET_PATH, figure_label, as_keypoints = False)
+
         np.savetxt(f'{BATCH_PATH}/batch_{initial_batch_id + idx}/tests_params/original_params.txt', M_ori)
         np.savetxt(f'{BATCH_PATH}/batch_{initial_batch_id + idx}/tests_params/kp1.txt', kp1_ori)
         np.savetxt(f'{BATCH_PATH}/batch_{initial_batch_id + idx}/tests_params/kp2.txt', kp2_ori)
